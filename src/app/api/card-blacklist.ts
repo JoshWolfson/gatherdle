732aa238d3d6273f9c77d0822f9f969c8b7376a4
JoshWolfson <joshscottwolfson@gmail.com>
2025-11-07 12:49:20 -0500

    add blacklist and loop to garuntee good card

---
+++ src/app/api/card-blacklist.ts
@@ -0,0 +1,7 @@
+export const cardBlacklist = [
+  "plains",
+  "island",
+  "swamp",
+  "mountain",
+  "forest",
+];
--- src/app/api/insert-daily-card/route.tsx
+++ src/app/api/insert-daily-card/route.tsx
@@ -2,36 +2,38 @@ import { NextResponse } from "next/server";
 import { Card } from "../card/card.interface";
 import db from "@/db";
 import { cards } from "@/db/schema";
+import { cardBlacklist } from "../card-blacklist";
 
 export async function insertDailyCard() {
   try {
-    // Fetch a random card from Scryfall
-    const response = await fetch("https://api.scryfall.com/cards/random");
-    if (!response.ok) {
-      return NextResponse.json(
-        { error: "Failed to fetch card" },
-        { status: 500 }
-      );
-    }
-    const card: Card = await response.json();
+    let newCard: Card | null = null;
+    while (newCard === null) {
+      const response = await fetch("https://api.scryfall.com/cards/random");
+      if (!response.ok) {
+        return NextResponse.json(
+          { error: "Failed to fetch card" },
+          { status: 500 }
+        );
+      }
+      const card: Card = await response.json();
 
-    // Check if the card already exists in the database (by Scryfall ID)
-    const existing = await db.query.cards.findFirst({
-      where: (cardTable, { eq }) => eq(cardTable.id, card.id),
-    });
+      // Check if the card already exists in the database (by Scryfall ID)
+      const existing = await db.query.cards.findFirst({
+        where: (cardTable, { eq }) => eq(cardTable.id, card.id),
+      });
 
-    if (existing) {
-      return NextResponse.json(
-        { message: "Card already exists", card: existing },
-        { status: 200 }
-      );
+      const isOnBlacklist = cardBlacklist.includes(card.name.toLowerCase());
+
+      if (!existing && !isOnBlacklist) {
+        newCard = card;
+      }
     }
 
     // Insert the card into the database
-    const newCard = await db.insert(cards).values(card).returning();
+    const savedCard = await db.insert(cards).values(newCard).returning();
 
     return NextResponse.json(
-      { message: "Card inserted", card: newCard },
+      { message: "Card inserted", card: savedCard },
       { status: 201 }
     );
   } catch (error) {
